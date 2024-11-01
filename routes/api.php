5ed1f47554849ba911db6c425a4b523e975609c0
Sabri Taieb <sabritaiebdev@gmail.com>
2024-11-01 09:37:20 +0100

    Make create event api only for logged in users

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,8 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::get('/events', [EventController::class, 'index']);
-Route::post('/events', [EventController::class, 'store']);
 Route::get('/events/{event}', [EventController::class, 'show']);
+Route::post('/events', [EventController::class, 'store'])->middleware('auth:sanctum');
 Route::put('/events/{event}', [EventController::class, 'update'])->middleware('auth:sanctum')->can('update', 'event');
 Route::patch('/events/{event}', [EventController::class, 'update'])->middleware('auth:sanctum')->can('update', 'event');
 Route::delete('/events/{event}', [EventController::class, 'destroy'])->middleware('auth:sanctum')->can('delete', 'event');
