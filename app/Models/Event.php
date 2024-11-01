62e70176321b7f2d767862ab513354bc3ba161e9
Sabri Taieb <sabritaiebdev@gmail.com>
2024-11-01 09:25:46 +0100

    Add soft delete functionality to events

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,10 +6,11 @@ use App\Enums\EventStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Event extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'user_id',
--- database/migrations/2024_11_01_065356_create_events_table.php
+++ database/migrations/2024_11_01_065356_create_events_table.php
@@ -26,6 +26,7 @@ return new class extends Migration
             $table->dateTime('start_at');
             $table->dateTime('end_at');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
