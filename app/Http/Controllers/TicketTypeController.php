ee8c2f20a37e38e8e1f30cc1d3362fdecc94a7df
Sabri Taieb <sabritaiebdev@gmail.com>
2024-11-01 10:03:01 +0100

    Add TicketType model

---
+++ app/Http/Controllers/TicketTypeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTicketTypeRequest;
+use App\Http\Requests\UpdateTicketTypeRequest;
+use App\Models\TicketType;
+
+class TicketTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTicketTypeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TicketType $ticketType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTicketTypeRequest $request, TicketType $ticketType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TicketType $ticketType)
+    {
+        //
+    }
+}
