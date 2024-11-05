11a086efec5dfa9eb5109b461661e6e9977f5c9a
Sabri Taieb <sabritaiebdev@gmail.com>
2024-11-05 17:34:53 +0100

    Update Home component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,21 @@
 <template>
     <main>
-        <h1 class="title">Latest events</h1>
+        <h1 class="title">Latest Events</h1>
 
         <div v-if="events.length">
             <div v-for="event in events" :key="event.id" class="border-l-4 border-black pl-4 mb-8">
+
                 <h2 class="font-bold text-xl">{{ event.name }}</h2>
+
                 <p class="text-xs text-slate-600 mb-4">
                     Created by <strong>{{ event.user.name }}</strong><br>
                     Status <strong>{{ event.status }}</strong>
                 </p>
                 <p>
                     {{ event.description }}
-                    <br>
-                    <RouterLink :to="{name: 'showEvent', params: {id: event.id}}" class="text-blue-500 underline">Go to
-                        event
-                    </RouterLink>
+                </p>
+                <p class="mt-3">
+                    <RouterLink :to="{name: 'showEvent', params: {id: event.id}}" class="bg-black hover:bg-gray-500 text-white px-4 py-2 rounded mt-2">Go to event</RouterLink>
                 </p>
             </div>
         </div>
